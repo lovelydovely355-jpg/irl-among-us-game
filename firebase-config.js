fbca34dd5a5e55d50959fa5bba83cc595aac5c31
lovelydovely355-jpg <lovelydovely355@gmail.com>
2026-06-26 20:50:25 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+firebase-config.js
+window.AMONG_ONLINE_FIREBASE_CONFIG = {
+apiKey: "AIzaSyDCpE5ay_RFCSttdXWEuPr3SCodIAYA5TY",
+authDomain: "irl-among-us-game.firebaseapp.com",
+databaseURL: "https://irl-among-us-game-default-rtdb.firebaseio.com",
+projectId: "irl-among-us-game",
+storageBucket: "irl-among-us-game.firebasestorage.app",
+messagingSenderId: "548143640673",
+appId: "1:548143640673:web:dfcae0408b5ff6070bba20"
+};
